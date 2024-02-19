974ca741979894f86908a8b7092e31ec11f3f600
GDog <harsanyi@windowslive.com>
2024-02-19 20:10:10 +0100

    new cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,40 +73,55 @@ require("lazy").setup({
 		-- Autocompletion
 		'hrsh7th/nvim-cmp',
 		dependencies = {
-			{ 'neovim/nvim-lspconfig' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'hrsh7th/cmp-cmdline' },
-			{ 'hrsh7th/nvim-cmp' }
-		},
-	}
+			-- Snippet Engine & its associated nvim-cmp source
+			{
+				'L3MON4D3/LuaSnip',
+				build = (function()
+					-- Build Step is needed for regex support in snippets
+					-- This step is not supported in many windows environments
+					-- Remove the below condition to re-enable on windows
+					if vim.fn.has 'win32' == 1 then
+						return
+					end
+					return 'make install_jsregexp'
+				end)(),
+			},
+			'saadparwaiz1/cmp_luasnip',
 
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+	},
 })
 require("catppuccin").setup({
- transparent_background = true
+	transparent_background = true
 })
 vim.cmd.colorscheme "catppuccin"
 
 require('lualine').setup({
-    options = {
-        section_separators = '', component_separators = '|',
-	icons_enabled = true,
-        theme = "catppuccin"
+	options = {
+		section_separators = '',
+		component_separators = '|',
+		icons_enabled = true,
+		theme = "catppuccin"
 
-    },
-    sections = {
-        -- left
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+	},
+	sections = {
+		-- left
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
 
-        -- right
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'searchcount' }
-    },
-    extensions = { 'fugitive'},
+		-- right
+		lualine_x = { 'encoding', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'searchcount' }
+	},
+	extensions = { 'fugitive' },
 })
 
 require('gitsigns').setup()
