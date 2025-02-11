1556fd1e9734441a69a247bc0caccbcead93d36d
harsanyiG <harsanyi@windowslive.com>
2025-02-11 18:08:44 +0100

    lsp - formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,5 +30,75 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
 
+-- Setup lazy.nvim
+require("lazy").setup({
+	spec = {
+		--- LSP ---
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		--- FORMATTER ---
+		"stevearc/conform.nvim",
+		--- MINI ---
+		{ "echasnovski/mini.nvim", version = "*" },
+	},
+	install = {
+		missing = true,
+		colorscheme = { "habamax" },
+	},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
 
+--- LSP CONFIG ---
+require("mason").setup()
+require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "ts_ls" } })
+
+--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local lspconfig = require("lspconfig")
+
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			format = { enable = true },
+		},
+	},
+})
+
+lspconfig.ts_ls.setup({})
+
+--- FORMATTER ---
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+vim.keymap.set("n", "<leader>f", function()
+	require("conform").format({ async = true, lsp_fallback = true })
+end)
+--- MINI ---
